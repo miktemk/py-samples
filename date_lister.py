de5aa5784d8e374b5b40658404df681b0d7f4e05
Mikhail Temkine <miktemk@gmail.com>
2016-07-21 15:06:05 -0400

    pretty

--- date_lister.py
+++ date_lister.py
@@ -4,10 +4,20 @@
 import sys, os, re
 import time, datetime
 
-daty = datetime.date.today()
+# .... constants
+doWeekendColor = True # .... change this if u like!
+weekendColor = "#ffdd99" # .... change this if u like!
+weekend = set([5, 6])
+
+# .... let's go!
+daty = datetime.date.today() # - datetime.timedelta(days=30)
 
 for i in range(365):
 	dateStr = "{:%a/%b %d/%Y}".format(daty)
+	if daty.weekday() == 0:
+		print ("----------------------------------------------------------------------\n")
+	if doWeekendColor and daty.weekday() in weekend:
+		dateStr = "<font color='{1}'>{0}</font>".format(dateStr, weekendColor)
 	print (dateStr)
 	print ("---\n\n\n")
 	daty += datetime.timedelta(days=1)
